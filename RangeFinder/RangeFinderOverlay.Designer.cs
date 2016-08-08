a8d5d0febbf485edd5ffcb7faabf26b23101ba22
Evan Ferris <evan.ferris@morningstar.com>
2016-08-08 10:27:34 +1000

    First commit

---
+++ RangeFinder/RangeFinderOverlay.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace RangeFinder
+{
+    partial class RangeFinderOverlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RangeFinderOverlay));
+            this.panHandle = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panHandle
+            // 
+            this.panHandle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panHandle.Location = new System.Drawing.Point(499, 450);
+            this.panHandle.Name = "panHandle";
+            this.panHandle.Size = new System.Drawing.Size(25, 23);
+            this.panHandle.TabIndex = 1;
+            this.panHandle.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panHandle_MouseClick);
+            this.panHandle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panHandle_MouseDown);
+            // 
+            // RangeFinderOverlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(720, 720);
+            this.Controls.Add(this.panHandle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RangeFinderOverlay";
+            this.Text = "Range Finder";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Load += new System.EventHandler(this.RangeFinderOverlay_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.RangeFinderOverlay_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panHandle;
+
+    }
+}
+
---
+++ RangeFinder/RangeFinderOverlay.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices; 
+using System.Text;
+using System.Windows.Forms;
+
+namespace RangeFinder
+{
+    public partial class RangeFinderOverlay : Form
+    {
+        #region Form Dragging API Support
+
+        //The SendMessage function sends a message to a window or windows. 
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        //ReleaseCapture releases a mouse capture 
+        [DllImportAttribute("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        public static extern bool ReleaseCapture();
+
+        #endregion 
+
+        #region Properties
+
+        public int PixelsInInch
+        { get; set; }
+
+        public int InchesToDraw
+        { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public RangeFinderOverlay()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// When the form is first loaded, this sets the initial parameters
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RangeFinderOverlay_Load(object sender, EventArgs e)
+        {
+            this.PixelsInInch = int.Parse(ConfigurationSettings.AppSettings["PixelsInInch"]);
+            this.InchesToDraw = int.Parse(ConfigurationSettings.AppSettings["InchesToDraw"]);
+
+            this.Width = (2 * PixelsInInch * InchesToDraw) + 2;
+            this.Height = (2 * PixelsInInch * InchesToDraw) + 2;
+
+            panHandle.Left = (Width / 2) + PixelsInInch;
+            panHandle.Top = (Width / 2) + PixelsInInch;
+        }
+
+        /// <summary>
+        /// This draws the actual ranges
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RangeFinderOverlay_Paint(object sender, PaintEventArgs e)
+        {
+            int centerX = PixelsInInch * InchesToDraw;
+            int centerY = PixelsInInch * InchesToDraw;
+
+            // Get the graphics object 
+            Graphics gfx = e.Graphics;
+
+            // Create a new pen that we shall use for drawing the line 
+            Pen mainPen = new Pen(Color.Lime);
+            Pen secondaryPen = new Pen(Color.Gray);
+
+            // Draw circles marking the distance from the center in 1 inch increments
+            for (int i = 3; i <= 12; i++)
+            {
+                Pen pen;
+                if (i % 2 == 0)
+                    pen = mainPen;
+                else
+                    pen = secondaryPen;
+
+                gfx.DrawEllipse(pen, centerX - (i * PixelsInInch), centerY - (i * PixelsInInch), i * PixelsInInch * 2, i * PixelsInInch * 2);
+            }
+
+            // Draw 4 lines to point to the center
+            gfx.DrawLine(mainPen, new Point(centerX, centerY + 3), new Point(centerX, centerY + 10));
+            gfx.DrawLine(mainPen, new Point(centerX + 3, centerY), new Point(centerX + 10, centerY));
+            gfx.DrawLine(mainPen, new Point(centerX, centerY - 3), new Point(centerX, centerY - 10));
+            gfx.DrawLine(mainPen, new Point(centerX - 3, centerY), new Point(centerX - 10, centerY));
+
+            //Draw the center dot
+            Bitmap bm = new Bitmap(1,1);
+            bm.SetPixel(0, 0, mainPen.Color);
+            gfx.DrawImageUnscaled(bm,centerX, centerY);
+
+            //Draw the range numbers
+            Font font = new Font("Verdana", 10, FontStyle.Bold, GraphicsUnit.Point);
+            int spacing = -3;
+
+            for (int i = 3; i <= 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    SizeF size = gfx.MeasureString(i.ToString(), font);
+                    int dist = (int)(Math.Ceiling(Math.Sqrt((i * PixelsInInch) * (i * PixelsInInch) / 2)));
+
+                    gfx.DrawString(i.ToString(), font, Brushes.Lime, centerX + dist + spacing, centerY - dist - size.Height - spacing);
+                    gfx.DrawString(i.ToString(), font, Brushes.Lime, centerX - dist - size.Width - spacing, centerY + dist + spacing);
+                }
+            }            
+        }
+
+        /// <summary>
+        /// This moves the overlay around the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panHandle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, 0xa1, 0x2, 0);
+            } 
+        }
+
+        /// <summary>
+        /// This handles commands that the user gives to the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panHandle_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+                this.Dispose();
+            else if (e.Button == MouseButtons.Right)
+                WindowState = FormWindowState.Minimized;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
